85efad278b667cef5b1284c0eaba4edf97ed679d
Roman Sereda <sereda.roman@gmail.com>
2014-10-08 18:52:45 +0300

    first version 1.0

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
 HLS-Segmenter-AWS-S3
 ====================
 
-mp4 video to HLS and upload to Amazon S3 bucket
+The is script for conver mp4 video file from source folder  to HLS streams and upload this stream to  to Amazon S3 bucket
+
+
+install requrement
+
+for script work you need boto modules for AWS 
+
+#pip install boto
+
+more datail about boto instalation -
+http://boto.readthedocs.org/en/latest/getting_started.html
+
+also script requred ffmpeg in you system
+Best values compiled latest version from source
+
+The is instruction for this
+
+https://trac.ffmpeg.org/wiki/CompilationGuide
+
+or simple install from repositories
+
+Ubuntu debian
+#sudo apt-get install ffmpeg 
+
+CentOS
+#sudo yum install ffmpeg
+
+
+
+
+
+Configuration
+
+in config.json you need set AWS credential 
+path to source folder 
+path for temp folder
+path to ffmpeg
+path to ffprobe
+
+config in JSON format
+
+{
+"aws":{
+		"access_key":"AWS Access Key",
+		"secret_key":"AWS Secure Key",
+		"s3bucket": "S3 bucket name"},
+"tmp":"/tmp/tmp/",
+"source":"/home/video/",
+"log":"/home/log.log",
+"ffmpeg": "/usr/bin/ffmpeg",
+"ffprobe" : "/usr/bin/ffprobe"
+}
+
+
+Using
+
+simple run 
+./hls-segmenter.py
---
+++ hls-segmenter.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+#  upload to AWS S3 and clean up
+
+# author Roman Sereda
+# sereda.roman@gmail.com
+#
+# install dependenses
+ 
+#sudo pip install boto
+
+
+import json
+import os.path
+import logging
+import subprocess
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+
+config_file = 'config.json'
+json_data=open(config_file)
+config = json.load(json_data)
+json_data.close()
+
+logging.basicConfig(filename=config['log'],level=logging.DEBUG)
+
+
+###########################################################################
+def s3_file_upload (config,filename,keyname):
+	conn = S3Connection(config['aws']["access_key"],config['aws']["secret_key"])
+	mybucket = conn.get_bucket(config['aws']["s3bucket"]) # select bucket
+	k = Key(mybucket) # select key
+	k.key = keyname #named new key
+	k.set_contents_from_filename(filename) #upload new  file name
+	k.set_acl('public-read') # set publis read access
+	keylist = mybucket.list() # get list of files
+	result = False
+	ss= []
+	for key in keylist:
+		ss.append( key.name)
+	if any(keyname in s for s in ss):
+		logging.debug('s3_file_upload ' + 'Upload ' + keyname + "Completed")
+		result = result | True
+	rs = conn.close()
+	return result
+
+
+def isVideo (ffprobe,filename):
+	if os.path.isfile(filename):
+		command = [ ffprobe, "-v","quiet","-print_format","json","-show_format","-show_streams",  filename]
+		process = subprocess.Popen(command, stdout=subprocess.PIPE)
+		out, err = process.communicate()
+		video_stat = json.loads(out)
+		stat = []
+		print video_stat
+		if not len (video_stat) is 0 :
+			if 'streams' in video_stat:
+				logging.debug('isVideo ' + 'tested ' + filename )
+				if len (video_stat['streams']) > 2:
+					logging.debug('isVideo ' + 'tested the is Vidoe' + filename )
+					return video_stat
+		return False
+
+
+def s3_get_key_list (config):
+	conn = S3Connection(config["aws"] ["access_key"], config["aws"] ["secret_key"])
+	mybucket = conn.get_bucket(config["aws"] ["s3bucket"])
+	key_list = []
+	for key in mybucket.list():
+		key_list.append(key.name)
+	rs = conn.close()
+	return key_list
+
+
+
+def video_segmenter (ffmpeg, filepath , folder , stream_name):
+	if os.path.isfile(filepath):
+		command = [ ffmpeg, "-re" ,"-i",filepath,"-map","0","-codec:v","libx264","-codec:a","libfdk_aac","-codec:s", "copy", "-flags","-global_header","-f","segment","-segment_list",folder+"playlist.m3u8","-segment_time","10","-segment_format","mpegts",folder + "out%05d.ts"]
+		process = subprocess.Popen(command, stdout=subprocess.PIPE)
+		out, err = process.communicate()
+		print out
+		
+		
+def main (config):
+	filelist =sorted( os.listdir(config["source"]))
+	print filelist
+	for filename in filelist:
+		if isVideo (config["ffprobe"],config["source"] + filename):
+			if filename.find('.') is -1:
+				stream_name = filename.split('.',1)
+			else :
+				stream_name = filename
+			video_segmenter (config["ffmpeg"], config["source"] + filename , config["tmp"] , stream_name)
+			upload_list = sorted( os.listdir(config["tmp"]))
+			if "playlist.m3u8" in upload_list and len (upload_list) > 2:
+				 for ufile in upload_list:
+					 logging.debug('main ' + 'procesed ' + " " + ufile )
+					 if s3_file_upload (config,config["tmp"] + ufile, stream_name + "/" + ufile):
+						 logging.debug('main ' + "Upload " + stream_name + "/" + ufile + "################" )
+						 os.remove(config["tmp"] + ufile)
+			
+	print s3_get_key_list (config)
+
+
+
+if __name__ == "__main__":
+	main (config)
